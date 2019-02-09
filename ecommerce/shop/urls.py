1c26b989b0f7674e8c05803c37d6feb49d4184c1
Rogerio <rrgmax@bol.com.br>
2019-02-09 03:32:29 -0200

    add 'DIRS': [os.path.join(BASE_DIR, 'template')], into settings.py to fix error

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'shop'
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
-    url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
-    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    path('', views.product_list, name='product_list'),
+    path('(?P<category_slug>[-\w]+)/', views.product_list, name='product_list_by_category'),
+    path('(?P<id>\d+)/(?P<slug>[-\w]+)/', views.product_detail, name='product_detail'),
 ]
